e3a444e8a6fcd596f94d9bdcf28dbd962f347e02
Aman Sharma <amanharitsh123@gmail.com>
2017-10-20 10:47:09 +0530

    a complete change

---
+++ myscript.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".bg .btn-circle").hide(0).delay(200).fadeIn(3000)
+       $(".bg .btn-circle").click(function(){
+        $(".bg .btn-circle").fadeOut(1000,function(){$(".bg .jumbotron").fadeIn(1000);});
+               
+    });
+});
